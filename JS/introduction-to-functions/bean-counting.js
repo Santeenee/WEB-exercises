6ba8a9c36c8c57a2b9cd223f67ad553e355a52c3
Francesco Santini <francescotn02@gmail.com>
2022-02-04 10:22:39 +0100

    modified bean-counting exercise

--- JS/introduction-to-functions/bean-counting.js
+++ JS/introduction-to-functions/bean-counting.js
@@ -1,19 +1,24 @@
 
 console.log("\n\nExercise BEAN COUNTING\n----------------------\n")
 
-function countChar(str, symbol = 'B') {
-  let countSymbol = 0
+function countChar(str, character = 'B') {
+  let countChar = 0
 
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === symbol)
-      countSymbol++
+    if (str[i] === character) 
+      countChar++
   }
   //return how many uppercase symbols
-  return `${countSymbol} '${symbol}' in '${str}'`
+  return `${countChar} '${character}' in '${str}'`
 }
 
 console.log(countChar("barBagianni BBB"))  // -> 4
 console.log(countChar("barBagianni"))      // -> 1
 
-console.log(countChar("barBagianni", 'b')) // -> 0
-console.log(countChar("barBagianni", 'a')) // -> 3
+let character = "aa"
+do {
+  character = prompt("Inserisci 1 carattere da cercare: ")
+} while (character.length > 1 || !isNaN(character));
+
+console.log(countChar("Il barBagianni vive nel bosco", character)) // -> 0
+console.log(countChar("barBagianni", character)) // -> 3
