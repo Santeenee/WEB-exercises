679cd74be59486af8f73863417724f456096dcf6
Francesco Santini <francescotn02@gmail.com>
2022-02-10 21:02:10 +0100

    JS -> bike-project (somehow) finished

--- JS/bike-project/script.js
+++ JS/bike-project/script.js
@@ -1,4 +1,4 @@
-//* DATA, BINDINGS, VARIABLES
+//* DATA, BINDINGS, ARRAYS
 const bikes = [
   { id: 1, category: "Electric Bikes", name: "Rad Rover" },
   { id: 2, category: "Electric Bikes", name: "Rad Rover Step-thru" },
@@ -9,40 +9,36 @@ const bikes = [
 let cart = [] //[{idBike, quantity}, ...]
 const fullDayRates = [75, 35]
 
+//* CSS
+let h2 = 'font-size: x-large;' //large, x-large, xx-large
+let padding = 'padding: 10px 12px;'
+let margin = 'margin: 5px 0;'
+let radius = 'border-radius: 8px;'
+let warn = `background: yellow; color: black; ${padding} ${margin} ${radius}`
+
 //* FUNCTIONS
 function menu() {
   let menuToPrint =
-    '\nPress\n' +
+    "\n%cChoose%c\n" +
     "    1: bikes list\n" +
     "    2: add a bike to the cart\n" +
     "    3: show cart\n" +
     "    4: remove bike from cart\n" +
     "    5: checkout\n" +
-    "other: exit\n\n"
-  console.log(menuToPrint)
-  // console.log('\nPress')
-  // console.log("    1: bikes list");
-  // console.log("    2: add a bike to the cart");
-  // console.log("    3: show cart");
-  // console.log("    4: remove bike from cart");
-  // console.log("    5: checkout");
-  // console.log("other: exit\n");
+    "other: exit\n"
+  console.log(menuToPrint, `font-weight: bold`, '')
 }
 
 function printBikes() {
-  let toPrint = ''
   console.clear()
-  // console.log(`\nBIKES LIST`)
-  console.log("%cBIKES LIST", "font-size: x-large");
+  console.log("%cBIKES LIST", h2);
 
-  // is bikes array empty?
   if (bikes.length === 0) {
     console.warn('There are no bikes')
     return //better than using 'else'
   }
 
-  toPrint += '┌───────────────────────────────────────────────────────────────\n'
-  // console.table(bikes)
+  let toPrint = '┌───────────────────────────────────────────────────────────────\n'
   let dailyPrice;
   for (let bike of bikes) {
 
@@ -63,42 +59,50 @@ function printBikes() {
 }
 
 function addToCart() {
-  printBikes()
-  let bikeChoice = prompt('Scegli bici tramite ID', '1')
-  let quantity = Number(prompt('Scegli QUANTITA', '2'))
-  let isInCart = false
 
+  printBikes()
+
+  let x = 0 // = bikeChoice
+  do {
+    x = Number(prompt('Choose bike to ADD typing its ID (1-4)', '1'))
+  } while (!(x >= 1 && x <= 4) || isNaN(x));
+  let bikeChoice = x
+
+  let quantity = 0
+  do {
+    quantity = Number(prompt('Choose quantity', '2'))
+  } while (quantity <= 0 || isNaN(quantity));
+
+  let isInCart = false
   for (let item of cart) {
+
     //if (item already exists in cart)
     //then increment quantity
     if (bikeChoice == item.idBike) {
-      item.quantity += Number(quantity)
+      item.quantity += quantity
       isInCart = true
     }
   }
   if (!isInCart) {
     cart.push({ 'idBike': bikeChoice, 'quantity': quantity })
   }
+  console.log('%cItems added to cart', `background: green; color: black; ${padding} ${margin} ${radius}`)
 }
 
 function showCart() {
-  let toPrint = ''
   console.clear()
-  // console.log('CART')
-  console.log("%cCART", "font-size: x-large");
-
+  console.log("%cCART", h2);
 
   if (cart.length === 0) {
-    // console.warn("the cart is empty")
-    console.log('%c The cart is empty ', 'background: yellow; color: black; padding: 10px;');
+    console.log('%cThe cart is empty', warn);
     return
   }
 
-  toPrint += '┌─────────────────────────────────\n'
+  let toPrint = '┌─────────────────────────────────\n'
   for (let item of cart) {
     for (let bike of bikes) {
       if (item.idBike == bike.id) {
-        toPrint += `│ ${item.quantity} "${bike.name}"\n`
+        toPrint += `│ ${item.quantity} x "${bike.name}"\n`
       }
     }
   }
@@ -108,19 +112,24 @@ function showCart() {
 
 function removeFromCart() {
   printBikes()
-  if (cart.length == 0) {
-    // console.warn("the cart is empty")
-    console.log('%c The cart is empty ', 'background: yellow; color: black');
+  if (cart.length === 0) {
+    console.log('%c The cart is empty ', warn);
     return
   }
-  let bikeChoice = prompt('Choose bike to remove typing ID', '4')
-  //TODO let quantity = prompt('Scegli QUANTITA', '2')
+
+  let x = 0
+  do {
+    x = Number(prompt('Choose bike to REMOVE typing its ID (1-4)', '4'))
+  } while (!(x >= 1 && x <= 4) || isNaN(x));
+  let bikeChoice = x
 
   for (let i = 0; i < cart.length; i++) {
     for (let j = 0; j < bikes.length; j++) {
-      if (cart[i].idBike == bikeChoice && cart[i].idBike == bikes[j].id) { //??????????????????????????
-        console.warn(`${cart[i].quantity} "${bikes[j].name}" removed`)
-        cart.splice(i, 1)
+      if (cart.length != 0) {
+        if (cart[i].idBike == bikeChoice && cart[i].idBike == bikes[j].id) {
+          console.log(`%c${cart[i].quantity} x "${bikes[j].name}" removed`, warn)
+          cart.splice(i, 1)
+        }
       }
     }
   }
@@ -128,7 +137,8 @@ function removeFromCart() {
 
 function checkout() {
   showCart()
-  //calcolo totale money
+
+  //tot money
   let dailyPrice = 0;
   let moneyForTypeOfBike = 0;
   let total = 0;
@@ -153,20 +163,19 @@ function checkout() {
     }
     total += moneyForTypeOfBike
   }
-  console.log(`Total amount: %c${total}$`, "background: hsl(200 100% 20%); font-weight: bold; padding: 10px;")
+  console.log(`Total amount: %c${total}$`, `background: hsl(200 100% 20%); font-weight: bold; ${padding} ${margin} ${radius}`)
 }
 
 
 //* EXECUTE
-let scelta = 1
-while (scelta == 1 || scelta == 2 || scelta == 3 || scelta == 4 || scelta == 5) {
+let userChoice = 1
+while (userChoice >= 1 && userChoice <= 5) {
 
   menu()
 
-  scelta = Number(prompt('Scegli una voce dal menu', "1"))
-  //console.clear()
+  userChoice = parseInt(prompt('Choose menu item', "1"), 10)
 
-  switch (scelta) {
+  switch (userChoice) {
     case 1:
       printBikes()
       break;
@@ -177,7 +186,7 @@ while (scelta == 1 || scelta == 2 || scelta == 3 || scelta == 4 || scelta == 5) 
       showCart() //TODO ADD PRICES... somehow
       break;
     case 4:
-      removeFromCart() //! it DOESN'T work
+      removeFromCart()
       break;
     case 5:
       checkout()
@@ -185,4 +194,6 @@ while (scelta == 1 || scelta == 2 || scelta == 3 || scelta == 4 || scelta == 5) 
   }
 }
 
-console.log('%cHave a nice day (￣︶￣)', 'background: green; padding: 10px')
+console.clear()
+checkout()
+console.log('%cBye, have a nice day (￣︶￣)', `background: green; ${padding} ${margin} ${radius}`)
